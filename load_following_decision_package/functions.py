71d3310a2fe3a732cd13f0ea69e26c1c56a0f4be
Christopher Curl <curlc@oregonstate.edu>
2022-01-20 12:34:36 -0800

    Removed params for calc_fuel_use function
    Since the operation parameters for mCHP and other system are defined in
    classes, I have removed input parameters from the calc_fuel_use
    function. I have also made minor edits to the file description for
    clarity.

--- load_following_decision_package/functions.py
+++ load_following_decision_package/functions.py
@@ -1,5 +1,5 @@
 """
-Calculates the optimal load dispatch for a CHP system
+Calculates the economics for electrical and thermal load following
  Assumptions:
  Fuel = NG
  Prime Mover = ICE
@@ -8,6 +8,7 @@ Calculates the optimal load dispatch for a CHP system
 """
 
 import classes
+import command_line
 from pint import UnitRegistry as ureg
 
 
@@ -15,14 +16,7 @@ def calc_fuel_use():    # Create class to track this?
     """
     Calculates the amount of fuel used by the auxiliary boiler and mCHP package.
     Requires part-load efficiency data for the mCHP system, electrical and thermal
-    load profiles for the building, and the HHV value for the fuel.
-    :param part_load_eff: Array of part-load conditions and corresponding electrical
-    efficiency values
-    :param heat_power_ratio: Ratio of heat produced by the mCHP package per unit of
-    electricity produced
-    :param thermal_load_profile: Thermal demand for the building (annual)
-    :param electrical_load_profile: Electrical demand for the building (annual)
-    :param fuel_HHV: Fuel HHV value
+    load profiles for the building, heat to power ratio, and the HHV value for the fuel.
     :return: Sum of annual fuel use
     """
     return None
