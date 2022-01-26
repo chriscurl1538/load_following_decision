4ef3a7a362031cf1484e6e4713f514ab6cbacb76
Christopher Curl <christopher.r.curl@gmail.com>
2022-01-25 16:59:00 -0800

    Load profiles imported in Building class
    used pandas to select specific columns from
    input_load_profiles_hourly.csv file and import as data frames. Selected
    days, months, hours, and day type columns as well as total electricity
    load and total thermal energy load columns. Also removed part-load
    efficiency np array from CHP class in favor of using .yaml input file
    with the command line.

--- load_following_decision_package/classes.py
+++ load_following_decision_package/classes.py
@@ -3,14 +3,13 @@ Initializes characteristics of equipment
 """
 
 import numpy as np
-import pandas
-from pint import UnitRegistry as ureg
+import pandas as pd
 
 
 class CHP:
-    def __init__(self, capacity=0, heat_power=0, turn_down_ratio=0):
+    def __init__(self, capacity=0, heat_power=0, turn_down_ratio=0, part_load=0):
         """
-        Defines the operating params of the micro-cogeneration system.
+        Defines the operating params of the mCHP system.
         Part-load efficiency data is sourced from the US EPA CHP Partnership's
         "Catalog of CHP Technologies, Section 2: Technology Characterization -
         Reciprocating Internal Combustion Engines". Data is specific to the
@@ -21,14 +20,7 @@ class CHP:
         self.cap = capacity
         self.hp = heat_power
         self.td = turn_down_ratio
-        self.part_load = np.array([[30, 34.4],
-                          [40, 37.9],
-                          [50, 40.7],
-                          [60, 42.0],
-                          [70, 42.7],
-                          [80, 43.7],
-                          [90, 44.9],
-                          [100, 45.7]])
+        self.pl = part_load
 
 
 class AuxBoiler:
@@ -38,26 +30,14 @@ class AuxBoiler:
         self.td = turn_down_ratio
 
 
-class Building:
-    def __init__(self, heat_load, electrical_load, net_metering=False):
-        self.hl = heat_load
-        self.el = electrical_load
-        self.nm = net_metering
-
-    def import_load_profile(self, file_name_el, file_name_hl):
-        """
-        Imports csv files with the annual electrical and thermal load profiles.
-        This is sourced from an eQuest energy model for a mid-rise apartment building.
-        :param file_name_el: String with file name of the electrical load profile
-        :param file_name_hl: String with file name of the heating load profile
-        :return: None
-        """
-        df_el = pandas.read_csv('{}.csv'.format(file_name_el))
-        print(df_el)
+class Building:     # TODO: Sum hourly load data to get monthly data
+    def __init__(self, file_name, net_metering=False):
+        df = pd.read_csv('{}.csv'.format(file_name))
+        df_el = df.iloc[8:, [0, 1, 2, 3, 16]]
         self.el = df_el
-        df_hl = pandas.read_csv('{}.csv'.format(file_name_hl))
-        print(df_hl)
+        df_hl = df.iloc[8:, [0, 1, 2, 3, 29]]
         self.hl = df_hl
+        self.nm = net_metering
 
 
 class TES:  # TODO: is a rate of charge or rate of discharge needed?
@@ -79,3 +59,10 @@ class EnergyWasted:
     def __init__(self, heat_wasted=0, electricity_wasted=0):
         self.hw = heat_wasted
         self.ew = electricity_wasted
+
+
+if __name__ == '__main__':
+    x = Building(file_name="input_load_profiles_hourly")
+    heat_load = x.hl
+    electrical_load = x.el
+    print(heat_load)
