487a770ceba8394cac7a4029b3ade906ddaf17a3
Christopher Curl <christopher.r.curl@gmail.com>
2022-02-10 10:43:28 -0800

    Created /docs folder and moved /source and /build into it

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,8 +55,8 @@ napoleon_attr_annotations = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to docs directory, that match files and
-# directories to ignore when looking for docs files.
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
