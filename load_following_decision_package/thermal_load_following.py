0344cdca3ba2ad560ee369fc01f7e09fdfa8da34
Christopher Curl <curlc@oregonstate.edu>
2022-02-17 16:44:36 -0800

    added one commented out function that may be useful for TLF logic

--- load_following_decision_package/thermal_load_following.py
+++ load_following_decision_package/thermal_load_following.py
@@ -15,49 +15,62 @@ from load_following_decision_package import classes
 import numpy as np
 
 
-def get_class_info():
-    """
-    Retrieves data stored in classes from classes.py for use in this module.
-
-    This function prevents classes from needing to be called in every function.
-
-    Returns
-    -------
-    heat_demand_hourly: numpy.ndarray
-        contains 8760 rows and 1 column. Items in the array must be convertible to float.
-    chp_cap: float
-        Size of the CHP system in kW (kilowatts)
-    chp_min: float
-        The minimum load of the system as a percentage of chp_cap. Calculated from the
-        turn-down ratio
-    chp_pl: numpy.ndarray
-        Array of part-loads (first col) and their associated part-load efficiencies
-        (second col)
-    fuel_cost: float
-        Cost of fuel in units of $/hr
-    """
-    energy_demand = classes.EnergyDemand()
-    if energy_demand.demand_file_name == "default_file.csv":
-        return None
-    else:
-        fuel_cost = energy_demand.fuel_cost
-        heat_demand_hourly = energy_demand.hl
-
-        chp = classes.CHP()
-
-        chp_cap = chp.cap
-        chp_pl = chp.pl
-
-        try:
-            chp_min = chp_cap / chp.td
-        except ZeroDivisionError:
-            chp_min = 0
-
-        return heat_demand_hourly, chp_cap, chp_min, chp_pl, fuel_cost
-
-
 def is_aux_boiler_needed():
     return None
+
+
+# def calc_aux_boiler_output():
+#     """
+#     Using CHP turn down ratio and heat to power ratio, this function determines when the
+#     heat demand exceeds the heat produced by the electric load following CHP system.
+#
+#     Returns
+#     -------
+#
+#     """
+#     chp = classes.CHP()
+#     chp_cap = chp.cap
+#     try:
+#         chp_min = chp_cap / chp.td
+#     except ZeroDivisionError:
+#         chp_min = 0
+#     chp_heat_min = chp_min * chp.hp
+#     chp_heat_max = chp_cap * chp.hp
+#
+#     demand = classes.EnergyDemand()
+#     heat_demand_hourly = demand.hl
+#
+#     ab = classes.AuxBoiler()
+#     ab_min = ab.cap / ab.td
+#
+#     boiler_needed = []
+#     boiler_heat = []
+#
+#     for i in range(heat_demand_hourly.shape[0]):
+#         # Verifies input dtype
+#         heat_demand = float(heat_demand_hourly[i])
+#
+#         # Verifies acceptable input value range
+#         assert heat_demand >= 0
+#
+#         if chp_heat_min <= heat_demand <= chp_heat_max:
+#             heat = 0
+#             boiler_heat.append(heat)
+#             boiler_needed.append(False)
+#         elif ab_min < heat_demand < chp_heat_min:
+#             heat = abs(heat_demand - ab_min)
+#             boiler_heat.append(heat)
+#             boiler_needed.append(True)
+#         elif chp_heat_max < heat_demand:
+#             heat = abs(heat_demand - chp_heat_max)
+#             boiler_heat.append(heat)
+#             boiler_needed.append(True)
+#         else:
+#             raise Exception(
+#                 "Error in ELF calc_aux_boiler_output function: Heat demand is less than minimum allowed aux "
+#                 "boiler output")
+#
+#     return boiler_needed, boiler_heat
 
 
 def calculate_ab_fuel_use():
