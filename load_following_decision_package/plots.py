fd34a012eedda2893757924287361d79fd3bf99f
Christopher Curl <curlc@oregonstate.edu>
2022-03-09 12:41:58 -0800

    added plots for chp electricity generated and chp heat generated. y-axis label now references units via pint

--- load_following_decision_package/plots.py
+++ load_following_decision_package/plots.py
@@ -4,19 +4,19 @@ Module Description:
     as well as relevant data calculated by the program
 
 Plots to be included:
-    Electrical Demand
-    Thermal Demand
-    mCHP Electricity Generated
-    mCHP Heat Generated
+    Electrical Demand (done)
+    Thermal Demand (done)
+    mCHP Electricity Generated (done)
+    mCHP Heat Generated (done)
     TES Storage/Dispatch History
     Aux Boiler Heat Generated
 """
 
-# TODO: plot electrical and thermal demand of building
-
 import matplotlib.pyplot as plt
 import numpy as np
 import classes
+import chp as cogen
+from __init__ import ureg
 
 
 def plot_electrical_demand():
@@ -47,6 +47,41 @@ def plot_thermal_demand():
     plt.show()
 
 
+def plot_chp_electricity_generated():
+    data = cogen.calc_hourly_generated_electricity()
+
+    # Convert to base units before creating numpy array for plotting
+    y = np.array([gen.to_base_units().magnitude for gen in data])
+
+    plt.plot(y)
+    plt.title('CHP Electricity Generation, Hourly')
+    plt.ylabel('Electricity [{}]'.format(data[0].units))
+    plt.yticks(np.arange(y.min(), y.max(), 5))
+    plt.xlabel('Time (hours)')
+
+    # plot
+    plt.show()
+
+
+def plot_chp_heat_generated():
+    data = cogen.calc_hourly_heat_generated()
+
+    # Convert to base units before creating numpy array for plotting
+    y = np.array([heat.to_base_units().magnitude for heat in data])
+
+    plt.plot(y)
+    plt.title('CHP Heat Generated, Hourly')
+    plt.ylabel('Heat [{}}]'.format(data[0].units))
+    plt.yticks(np.arange(y.min(), y.max(), 5))
+    plt.xlabel('Time (hours)')
+
+    # plot
+    plt.show()
+
+
+def plot_tes_history():
+    return None
+
+
 if __name__ == "__main__":
-    plot_thermal_demand()
-    plot_electrical_demand()
+    plot_chp_heat_generated()
