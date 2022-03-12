8c2244bf8c79fb1646574ff74e40d31fb3a260d2
Christopher Curl <curlc@oregonstate.edu>
2022-03-11 16:29:50 -0800

    initial commit

---
+++ load_following_decision_package/tests/test_aux_boiler.py
@@ -0,0 +1,21 @@
+"""
+Module Description:
+    Contains  test functions for functions located in the aux_boiler.py file
+
+NOT READY FOR TESTING
+"""
+
+import pytest
+from load_following_decision_package import aux_boiler as boiler
+
+
+def test_calc_aux_boiler_output():
+    return None
+
+
+def test_calc_annual_fuel_use():
+    return None
+
+
+def test_calc_annual_fuel_cost():
+    return None
