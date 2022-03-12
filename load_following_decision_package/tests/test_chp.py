3bd8ceda3c4e6485c4e5bda38049c2a7d3f39102
Christopher Curl <curlc@oregonstate.edu>
2022-03-11 16:34:05 -0800

    Deleted contents since major revisions are needed

--- load_following_decision_package/tests/test_chp.py
+++ load_following_decision_package/tests/test_chp.py
@@ -1,50 +1,37 @@
 """
 Module Description:
-    Contains  test functions for functions located in the "chp.py" file
+    Contains  test functions for functions located in the chp.py file
 
 NOT READY FOR TESTING
 """
 
 import pytest
-from load_following_decision_package import classes
-
-from load_following_decision_package import chp as elf
-
-import numpy as np
+from load_following_decision_package import chp as cogen
 
 
-# # TODO: Update data stored in class to modify hourly demand data for testing
-# def test_is_electric_utility_needed():
-#     # Test default input
-#     assert elf.calc_utility_electricity_needed() is None
-#
-#     # Test input array dtype
-#     a = np.ones(shape=(8759, 1))
-#     demand_dtype = np.append(a, ["demand"])
-#     exp_dtype = ValueError
-#     obs_dtype = is_electric_utility_needed(demand_hourly=demand_dtype)
-#     assert exp_dtype == obs_dtype
-#
-#     # Test acceptable demand values
-#     a = np.ones(shape=(8759, 1))
-#     demand_range = np.append(a, [-40])
-#     exp_range = AssertionError
-#     obs_range = is_electric_utility_needed(demand_hourly=demand_range)
-#     assert exp_range == obs_range
-#
-#     # Test function outputs
-#     a = np.ones(shape=(8757, 1))
-#     demand_output = np.append(a, [[50], [45], [55]], axis=0)
-#     exp_output = [False, False, True]
-#     obs_output = is_electric_utility_needed(demand_hourly=demand_output)
-#     assert exp_output[-1] == obs_output[-1]
-#     assert exp_output[-2] == obs_output[-2]
-#     assert exp_output[-3] == obs_output[-3]
-#
+def test_calc_hourly_electricity_bought():
+    return None
 
-# def test_calculate_ELF_annual_electricity_cost():
-#     return None
-#
-#
-# def test_calculate_part_load_efficiency():
-#     return None
+
+def test_calc_annual_electric_cost():
+    return None
+
+
+def test_calc_hourly_efficiency():
+    return None
+
+
+def test_calc_hourly_generated_electricity():
+    return None
+
+
+def test_calc_hourly_heat_generated():
+    return None
+
+
+def test_calculate_annual_fuel_use():
+    return None
+
+
+def test_calc_annual_fuel_cost():
+    return None
