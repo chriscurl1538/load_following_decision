e6b708112bdff96ae409fb58108fb4fd529618ac
Christopher Curl <curlc@oregonstate.edu>
2022-03-16 16:31:28 -0700

    removed ureg data - all pint modules now refer to __init__ in the modules folder

--- lfd_package/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Set global unit registry for use throughout the software package
-"""
-
-from pint import UnitRegistry, set_application_registry
-ureg = UnitRegistry()
-set_application_registry(ureg)
-Q_ = ureg.Quantity
