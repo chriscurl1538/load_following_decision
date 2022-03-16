b60538e40c552bd59c72ad5dfff60add697c2cef
Christopher Curl <curlc@oregonstate.edu>
2022-03-15 18:58:17 -0700

    created testing functions for all classes, in the process of removing dtype testing due to errors

--- lfd_package/tests/test_classes.py
+++ lfd_package/tests/test_classes.py
@@ -2,111 +2,104 @@
 Module description:
     This program tests the classes.py file
 """
-
-from lfd_package.classes import CHP
-from lfd_package.classes import AuxBoiler
-from lfd_package.classes import EnergyDemand
-# from lfd_package.classes import TES
-
 import numpy as np
-
-# TODO: Add tests for unacceptable inputs (negative numbers, capacity above 50, etc)
-
-
-# Testing CHP Class
-EMPTY_ARRAY = np.empty([10, 2])
-chp_test = CHP(capacity=50, heat_power=4, turn_down_ratio=3.3, part_load=EMPTY_ARRAY)
+from lfd_package.tests.__init__ import ureg
 
 
-def test_chp_class():
-    exp_cap = 50
-    obs_cap = chp_test.cap
-    assert exp_cap == obs_cap
-
-    exp_hp = 4
-    obs_hp = chp_test.hp
-    assert exp_hp == obs_hp
-
-    exp_td = 3.3
-    obs_td = chp_test.td
-    assert exp_td == obs_td
-
-    exp_pl = EMPTY_ARRAY
-    obs_pl = chp_test.pl
-    np.testing.assert_allclose(actual=obs_pl, desired=exp_pl, rtol=0.01)
+from lfd_package import classes
 
 
-# Testing AuxBoiler Class
-AB_test = AuxBoiler(capacity=50, efficiency=0.8, turn_down_ratio=3.3)
+def test_chp_class(class_info, chp_pl):
+    # Assign class values
+    chp = class_info[0]
+    assert isinstance(chp, classes.CHP)
+
+    assert 0 < chp.cap.magnitude <= 50
+    # assert chp.cap.unit == ureg.kW
+
+    assert 0 < chp.hp
+
+    assert 0 < chp.td
+
+    assert chp.pl == chp_pl
+    assert chp.pl.shape == (2, 8)
+
+    assert 0 < chp.cost.magnitude
+    # assert chp.cost.unit == 1/ureg.kW
+
+    assert 0 < chp.min.magnitude < chp.cap.magnitude
+    # assert chp.min.unit == ureg.kW
 
 
-def test_aux_boiler_class():
-    exp_cap = 50
-    obs_cap = AB_test.cap
-    assert exp_cap == obs_cap
+def test_aux_boiler_class(class_info):
+    # Assign class values
+    ab = class_info[1]
+    assert isinstance(ab, classes.AuxBoiler)
 
-    exp_eff = 0.8
-    obs_eff = AB_test.eff
-    assert exp_eff == obs_eff
+    cap_type = type(ab.cap.magnitude)
+    assert isinstance(cap_type, float)
+    assert 0 < ab.cap
+    # assert ab.cap.unit == ureg.Btu / ureg.hour
 
-    exp_td = 3.3
-    obs_td = AB_test.td
-    assert exp_td == obs_td
+    eff_type = type(ab.eff.magnitude)
+    assert isinstance(ab.eff, float)
+    assert 0 < ab.eff < 1
 
+    td_type = type(ab.td.magnitude)
+    assert isinstance(td_type, float)
+    assert 0 < ab.td
 
-# Testing EnergyDemand Class
-
-def test_energy_demand_class():
-    # TODO: Test that electrical and heating demands have been imported correctly
-    energy_demand = EnergyDemand()
-
-    # Verifies electrical load array size
-    rows_el = energy_demand.el.shape[0]
-    try:
-        cols_el = energy_demand.el.shape[1]
-    except IndexError:
-        cols_el = 1
-    assert rows_el == 8760
-    assert cols_el == 1
-
-    # Verifies heating load array size
-    rows_hl = energy_demand.hl.shape[0]
-    try:
-        cols_hl = energy_demand.hl.shape[1]
-    except IndexError:
-        cols_hl = 1
-    assert rows_hl == 8760
-    assert cols_hl == 1
-
-    # Verifies default values
-    assert energy_demand.demand_file_name == "default_file.csv"
-    assert energy_demand.el_cost == 0
-    assert energy_demand.fuel_cost == 0
-
-    # Verifies that values pass to class correctly
-    pass_values = EnergyDemand(file_name="name.csv", electric_cost=0.24, fuel_cost=0.5)
-    assert pass_values.el_cost == 0.24
-    assert pass_values.fuel_cost == 0.5
-    assert pass_values.demand_file_name == "name.csv"
+    min_type = type(ab.min.magnitude)
+    assert isinstance(min_type, float)
+    assert 0 < ab.min < ab.cap
+    # assert ab.min.unit == ureg.Btu / ureg.hour
 
 
-# # Testing TES Class
-# tes_test = TES(capacity=50, state_of_charge=0.5, charge=True, discharge=True)
+def test_energy_demand_class(class_info):
+    # Assign class values
+    demand = class_info[2]
+    assert isinstance(demand, classes.EnergyDemand)
+
+    assert isinstance(demand.demand_file_name, str)
+
+    assert isinstance(demand.el_cost, float)
+    assert 0 < demand.el_cost
+    # assert demand.el_cost.unit == 1/ureg.kWh
+
+    assert isinstance(demand.fuel_cost, float)
+    assert 0 < demand.fuel_cost
+    # assert demand.fuel_cost.unit == 1/ureg.megaBtu
+
+    assert isinstance(demand.hl, np.ndarray)
+    assert isinstance(demand.hl[0], float)
+    assert demand.hl.shape[0] == 8760
+    assert demand.hl.size == 8760
+    # assert demand.hl.unit == ureg.Btu
+
+    assert isinstance(demand.el, np.ndarray)
+    assert isinstance(demand.el[0], float)
+    assert demand.el.shape[0] == 8760
+    assert demand.el.size == 8760
+    # assert demand.el.unit == ureg.kWh
+
+    assert isinstance(demand.annual_el, float)
+    assert 0 < demand.annual_el
+    # assert demand.annual_el.unit == ureg.kWh
+
+    assert isinstance(demand.annual_hl, float)
+    assert 0 < demand.annual_hl
+    # assert demand.annual_hl.unit == ureg.Btu
 
 
-# def test_TES_class():
-#     exp_cap = 50
-#     obs_cap = tes_test.cap
-#     assert exp_cap == obs_cap
-#
-#     exp_soc = 0.5
-#     obs_soc = tes_test.soc
-#     assert exp_soc == obs_soc
-#
-#     exp_charge = True
-#     obs_charge = tes_test.charge
-#     assert exp_charge == obs_charge
-#
-#     exp_discharge = True
-#     obs_discharge = tes_test.discharge
-#     assert exp_discharge == obs_discharge
+def test_tes_class(class_info):
+    # Assign class values
+    tes = class_info[3]
+    assert isinstance(tes, classes.TES)
+
+    assert isinstance(tes.cap, float)
+    assert 0 < tes.cap
+    # assert tes.cap.unit == ureg.Btu
+
+    assert isinstance(tes.cost, float)
+    assert 0 < tes.cost
+    # assert tes.cost.unit == 1/ureg.kWh
