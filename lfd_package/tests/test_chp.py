07d010061900203469899d5f14eeaf2411540c8f
Christopher Curl <curlc@oregonstate.edu>
2022-03-16 19:38:57 -0700

    added commented out test, removed finished todo items

--- lfd_package/tests/test_chp.py
+++ lfd_package/tests/test_chp.py
@@ -6,10 +6,6 @@ Module Description:
 from lfd_package.modules import chp as cogen
 
 
-# TODO: test units
-# TODO: test passing bad values in (namely None or trying to put negative numbers in the yaml file)
-
-
 def test_calc_hourly_electricity_bought(class_info):
     chp = class_info[0]
     demand = class_info[2]
@@ -63,10 +59,12 @@ def test_calc_hourly_heat_generated(class_info):
         assert 0 <= i.magnitude
 
 
-# def test_calculate_annual_fuel_use(class_info):
-#     chp = class_info[0]
-#     demand = class_info[2]
-#     func = cogen.calculate_annual_fuel_use(chp=chp, demand=demand)
+def test_calculate_annual_fuel_use(class_info):
+    chp = class_info[0]
+    demand = class_info[2]
+    func = cogen.calculate_annual_fuel_use(chp=chp, demand=demand)
+
+    assert 0 <= func
 
 
 def test_calc_annual_fuel_cost(class_info):
@@ -76,4 +74,5 @@ def test_calc_annual_fuel_cost(class_info):
 
     demand_mmbtu = sum(demand.hl) * 1000000
     max_cost = demand_mmbtu * demand.fuel_cost
+
     assert 0 <= func <= max_cost
