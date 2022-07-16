3ce5941d1a8e94f82fae2ad4b449885af1328b48
Christopher Curl <christopher.r.curl@gmail.com>
2022-07-15 18:13:52 -0700

    updated function names

--- lfd_package/tests/test_chp.py
+++ lfd_package/tests/test_chp.py
@@ -6,15 +6,20 @@ Module Description:
 from lfd_package.modules import chp as cogen
 
 
-def test_calc_hourly_electricity_bought(class_info):
+def test_calc_electricity_bought_and_generated(class_info):
     chp = class_info[0]
     demand = class_info[2]
-    func = cogen.calc_hourly_electricity_bought(chp=chp, demand=demand)
+    func = cogen.calc_electricity_bought_and_generated(chp=chp, demand=demand)
 
-    assert isinstance(func, list)
-    assert len(func) == 8760
-    assert 0 < sum(func)
-    for index, i in enumerate(func):
+    assert isinstance(func[0], list)
+    assert len(func[0]) == 8760
+    assert 0 < sum(func[0])
+    for index, i in enumerate(func[0]):
+        assert 0 <= i <= demand.el[index]
+
+    assert isinstance(func[1], list)
+    assert len(func[1]) == 8760
+    for index, i in enumerate(func[1]):
         assert 0 <= i <= demand.el[index]
 
 
@@ -35,17 +40,6 @@ def test_calc_hourly_efficiency(class_info):
     assert len(func) == 8760
     for i in func:
         assert 0 < i < 1
-
-
-def test_calc_hourly_generated_electricity(class_info):
-    chp = class_info[0]
-    demand = class_info[2]
-    func = cogen.calc_hourly_generated_electricity(chp=chp, demand=demand)
-
-    assert isinstance(func, list)
-    assert len(func) == 8760
-    for index, i in enumerate(func):
-        assert 0 <= i <= demand.el[index]
 
 
 def test_calc_hourly_heat_generated(class_info):
