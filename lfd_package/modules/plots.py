e948ee339e0ecafae6878796d4675cee2ac5d8ca
Christopher Curl <christopher.r.curl@gmail.com>
2022-07-18 11:31:57 -0700

    Implemented TLF plots.
    Added TLF plots, updated ELF plot titles and function names, updated
    function names and arguments for functions called inside the plot
    functions.

--- lfd_package/modules/plots.py
+++ lfd_package/modules/plots.py
@@ -2,6 +2,8 @@
 Module Description:
     This module will create plots of data passed to the program
     as well as relevant data calculated by the program
+
+# TODO: Find bug that generates 7 plots
 """
 
 import matplotlib.pyplot as plt, numpy as np
@@ -9,9 +11,14 @@ from lfd_package.modules import thermal_storage as storage
 from lfd_package.modules import aux_boiler as boiler, chp as cogen
 
 
-def plot_electric(chp=None, demand=None):
-    data1 = cogen.calc_electricity_bought_and_generated(chp=chp, demand=demand)[1]  # generation
-    data2 = cogen.calc_electricity_bought_and_generated(chp=chp, demand=demand)[0]  # bought
+"""
+ELF Plots
+"""
+
+
+def elf_plot_electric(chp=None, demand=None):
+    data1 = cogen.elf_calc_electricity_bought_and_generated(chp=chp, demand=demand)[1]
+    data2 = cogen.elf_calc_electricity_bought_and_generated(chp=chp, demand=demand)[0]
 
     # Convert to base units before creating numpy array for plotting
     y0 = np.array([dem.to_base_units().magnitude for dem in demand.el])
@@ -34,7 +41,7 @@ def plot_electric(chp=None, demand=None):
 
     # Set up plot
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex='all', sharey='all')
-    fig.suptitle('Annual Electrical Demand and Generation, Daily [kWh]')
+    fig.suptitle('ELF Annual Electrical Demand and Generation, Daily [kWh]')
     ax1.plot(daily_kwh_dem_array)
     ax1.set_ylabel('Demand')
     ax2.plot(daily_kwh_chp_array)
@@ -46,10 +53,10 @@ def plot_electric(chp=None, demand=None):
     plt.show()
 
 
-def plot_thermal(chp=None, demand=None, tes=None, ab=None):
-    data1 = cogen.calc_hourly_heat_generated(chp=chp, demand=demand)
-    data2 = storage.tes_heat_stored(chp=chp, demand=demand, tes=tes)[0]
-    data3 = boiler.calc_aux_boiler_output(chp=chp, demand=demand, tes=tes, ab=ab)
+def elf_plot_thermal(chp=None, demand=None, tes=None, ab=None):
+    data1 = cogen.elf_calc_hourly_heat_generated(chp=chp, demand=demand)
+    data2 = storage.calc_tes_heat_flow_and_soc(chp=chp, demand=demand, tes=tes, load_following_type="ELF")[0]
+    data3 = boiler.calc_aux_boiler_output(chp=chp, demand=demand, tes=tes, ab=ab, load_following_type="ELF")
 
     # Convert to base units before creating numpy array for plotting
     y0 = np.array([dem.to_base_units().magnitude for dem in demand.hl])
@@ -76,7 +83,7 @@ def plot_thermal(chp=None, demand=None, tes=None, ab=None):
 
     # Set up plot
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, sharex='all', sharey='all')
-    fig.suptitle('Annual Thermal Demand and Generation, Daily [Btu]')
+    fig.suptitle('ELF Annual Thermal Demand and Generation, Daily [Btu]')
     ax1.plot(daily_btu_dem_array)
     ax1.set_ylabel('Demand')
     ax2.plot(daily_btu_chp_array)
@@ -90,8 +97,8 @@ def plot_thermal(chp=None, demand=None, tes=None, ab=None):
     plt.show()
 
 
-def plot_tes_soc(chp=None, demand=None, tes=None):
-    data = storage.tes_heat_stored(chp=chp, demand=demand, tes=tes)[1]
+def elf_plot_tes_soc(chp=None, demand=None, tes=None):
+    data = storage.calc_tes_heat_flow_and_soc(chp=chp, demand=demand, tes=tes, load_following_type="ELF")[1]
 
     # Convert to base units before creating numpy array for plotting
     y = np.array([status.to_base_units().magnitude for status in data])
@@ -106,7 +113,7 @@ def plot_tes_soc(chp=None, demand=None, tes=None):
 
     # Set up plots
     plt.plot(daily_btu_array)
-    plt.title('TES SOC, Daily Avg')
+    plt.title('ELF TES SOC, Daily Avg')
     plt.ylabel('SOC')
     plt.yticks(np.arange(0, 1, 0.1))
     plt.xlabel('Time (days)')
@@ -114,3 +121,113 @@ def plot_tes_soc(chp=None, demand=None, tes=None):
     # plot
     plt.show()
 
+
+"""
+TLF Plots
+"""
+
+
+def tlf_plot_electric(chp=None, demand=None):
+    data1 = cogen.tlf_calc_electricity_bought_and_generated(chp=chp, demand=demand)[1]
+    data2 = cogen.tlf_calc_electricity_bought_and_generated(chp=chp, demand=demand)[0]
+
+    # Convert to base units before creating numpy array for plotting
+    y0 = np.array([dem.to_base_units().magnitude for dem in demand.el])
+    y1 = np.array([gen.to_base_units().magnitude for gen in data1])
+    y2 = np.array([gen.to_base_units().magnitude for gen in data2])
+
+    # Calculate daily sums
+    daily_kwh_dem = []
+    daily_kwh_chp = []
+    daily_kwh_buy = []
+
+    for i in range(24, len(y0) + 1, 24):
+        daily_kwh_dem.append(y0[(i - 24):i].sum())
+        daily_kwh_chp.append(y1[(i - 24):i].sum())
+        daily_kwh_buy.append(y2[(i - 24):i].sum())
+
+    daily_kwh_dem_array = np.array(daily_kwh_dem)
+    daily_kwh_chp_array = np.array(daily_kwh_chp)
+    daily_kwh_buy_array = np.array(daily_kwh_buy)
+
+    # Set up plot
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex='all', sharey='all')
+    fig.suptitle('TLF Annual Electrical Demand and Generation, Daily [kWh]')
+    ax1.plot(daily_kwh_dem_array)
+    ax1.set_ylabel('Demand')
+    ax2.plot(daily_kwh_chp_array)
+    ax2.set_ylabel('CHP')
+    ax3.plot(daily_kwh_buy_array)
+    ax3.set_ylabel('Electricity Bought')
+    ax3.set_xlabel('Time (days)')
+
+    plt.show()
+
+
+def tlf_plot_thermal(chp=None, demand=None, tes=None, ab=None):
+    data1 = cogen.tlf_calc_hourly_heat_generated(chp=chp, demand=demand)
+    data2 = storage.calc_tes_heat_flow_and_soc(chp=chp, demand=demand, tes=tes, load_following_type="TLF")[0]
+    data3 = boiler.calc_aux_boiler_output(chp=chp, demand=demand, tes=tes, ab=ab, load_following_type="TLF")
+
+    # Convert to base units before creating numpy array for plotting
+    y0 = np.array([dem.to_base_units().magnitude for dem in demand.hl])
+    y1 = np.array([gen.to_base_units().magnitude for gen in data1])
+    y2 = np.array([gen.to_base_units().magnitude for gen in data2])
+    y3 = np.array([gen.to_base_units().magnitude for gen in data3])
+
+    # Calculate daily sums
+    daily_btu_dem = []
+    daily_btu_chp = []
+    daily_btu_tes = []
+    daily_btu_ab = []
+
+    for i in range(24, len(y0) + 1, 24):
+        daily_btu_dem.append(y0[(i - 24):i].sum())
+        daily_btu_chp.append(y1[(i - 24):i].sum())
+        daily_btu_tes.append(y2[(i - 24):i].sum())
+        daily_btu_ab.append(y3[(i - 24):i].sum())
+
+    daily_btu_dem_array = np.array(daily_btu_dem)
+    daily_btu_chp_array = np.array(daily_btu_chp)
+    daily_btu_tes_array = np.array(daily_btu_tes)
+    daily_btu_ab_array = np.array(daily_btu_ab)
+
+    # Set up plot
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, sharex='all', sharey='all')
+    fig.suptitle('TLF Annual Thermal Demand and Generation, Daily [Btu]')
+    ax1.plot(daily_btu_dem_array)
+    ax1.set_ylabel('Demand')
+    ax2.plot(daily_btu_chp_array)
+    ax2.set_ylabel('CHP')
+    ax3.plot(daily_btu_tes_array)
+    ax3.set_ylabel('TES Discharge')
+    ax4.plot(daily_btu_ab_array)
+    ax4.set_ylabel('Aux Boiler')
+    ax4.set_xlabel('Time (days)')
+
+    plt.show()
+
+
+def tlf_plot_tes_soc(chp=None, demand=None, tes=None):
+    data = storage.calc_tes_heat_flow_and_soc(chp=chp, demand=demand, tes=tes, load_following_type="TLF")[1]
+
+    # Convert to base units before creating numpy array for plotting
+    y = np.array([status.to_base_units().magnitude for status in data])
+
+    # Calculate daily avg for discharge plot
+    daily_btu = []
+
+    for i in range(24, len(y) + 1, 24):
+        daily_btu.append(np.average(y[(i - 24):i]))
+
+    daily_btu_array = np.array(daily_btu)
+
+    # Set up plots
+    plt.plot(daily_btu_array)
+    plt.title('TLF TES SOC, Daily Avg')
+    plt.ylabel('SOC')
+    plt.yticks(np.arange(0, 1, 0.1))
+    plt.xlabel('Time (days)')
+
+    # plot
+    plt.show()
