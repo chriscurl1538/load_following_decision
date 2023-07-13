61ad073ec683740de97712ce5473fa31605e3b99
Christopher Curl <christopher.r.curl@gmail.com>
2023-07-13 11:16:08 -0700

    Updated class info to match class initialization in command_line.py

--- lfd_package/tests/conftest.py
+++ lfd_package/tests/conftest.py
@@ -22,36 +22,42 @@ def class_info():
     demand = classes.EnergyDemand(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                                   grid_efficiency=data['grid_efficiency'],
                                   winter_start_inclusive=data['winter_start_inclusive'],
-                                  summer_start_inclusive=data['summer_start_inclusive'])
+                                  summer_start_inclusive=data['summer_start_inclusive'],
+                                  sim_ab_efficiency=data["energy_plus_eff"])
     emissions_class = classes.Emissions(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                                         grid_efficiency=data['grid_efficiency'],
                                         summer_start_inclusive=data['summer_start_inclusive'],
-                                        winter_start_inclusive=data['winter_start_inclusive'])
+                                        winter_start_inclusive=data['winter_start_inclusive'],
+                                        sim_ab_efficiency=data["energy_plus_eff"])
     costs_class = classes.EnergyCosts(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                                       grid_efficiency=data['grid_efficiency'],
                                       winter_start_inclusive=data['winter_start_inclusive'],
                                       summer_start_inclusive=data['summer_start_inclusive'],
+                                      sim_ab_efficiency=data["energy_plus_eff"],
                                       meter_type_el=data['meter_type_el'], meter_type_fuel=data['meter_type_fuel'],
                                       schedule_type_el=data['schedule_type_el'],
                                       schedule_type_fuel=data['schedule_type_fuel'],
                                       master_metered_el=data['master_metered_el'],
                                       single_metered_el=data['single_metered_el'],
                                       master_metered_fuel=data['master_metered_fuel'],
-                                      single_metered_fuel=data['single_metered_fuel'])  # TODO: Implement
+                                      single_metered_fuel=data['single_metered_fuel'])
     chp = classes.CHP(file_name=data['demand_filename'], city=data['city'], state=data['state'],
-                      grid_efficiency=data['grid_efficiency'],
+                      grid_efficiency=data['grid_efficiency'], sim_ab_efficiency=data["energy_plus_eff"],
                       summer_start_inclusive=data['summer_start_inclusive'],
                       winter_start_inclusive=data['winter_start_inclusive'], turn_down_ratio=data['chp_turn_down'],
-                      cost=data['chp_installed_cost'])
+                      installed_cost=data['chp_installed_cost'])
     ab = classes.AuxBoiler(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                            grid_efficiency=data['grid_efficiency'],
                            summer_start_inclusive=data['summer_start_inclusive'],
-                           winter_start_inclusive=data['winter_start_inclusive'], efficiency=data['ab_eff'])
+                           winter_start_inclusive=data['winter_start_inclusive'],
+                           sim_ab_efficiency=data["energy_plus_eff"],
+                           efficiency=data['ab_eff'])
     tes = classes.TES(file_name=data['demand_filename'], city=data['city'], state=data['state'],
-                      grid_efficiency=data['grid_efficiency'],
+                      grid_efficiency=data['grid_efficiency'], sim_ab_efficiency=data["energy_plus_eff"],
                       summer_start_inclusive=data['summer_start_inclusive'],
                       winter_start_inclusive=data['winter_start_inclusive'], start=data['tes_init'],
-                      cost=data['tes_installed_cost'])
+                      size_cost=data['tes_size_cost'], rate_cost=data['tes_rate_cost'],
+                      energy_density=data['tes_energy_density'])
 
     class_dict = {
         "demand": demand,
@@ -63,21 +69,3 @@ def class_info():
     }
 
     return class_dict
-
-
-# @pytest.fixture
-# def chp_pl():
-#     import numpy as np
-#
-#     part_load_list = [
-#         [30, 0.344],
-#         [40, 0.379],
-#         [50, 0.407],
-#         [60, 0.420],
-#         [70, 0.427],
-#         [80, 0.437],
-#         [90, 0.449],
-#         [100, 0.457]
-#     ]
-#     part_load_array = np.array(part_load_list)
-#     return part_load_array
