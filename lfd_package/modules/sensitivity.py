2ff08e1d613585998fb92eac8c90782ab8ddd357
Christopher Curl <christopher.r.curl@gmail.com>
2023-07-21 14:58:34 -0700

    Created function make_param_list() for creating list of sensitivity analysis values to run in analysis

--- lfd_package/modules/sensitivity.py
+++ lfd_package/modules/sensitivity.py
@@ -7,8 +7,19 @@ Module Description:
     - TES Size
 """
 
-from sensitivity import SensitivityAnalyzer
+# from sensitivity import SensitivityAnalyzer as sa
 
 
-def get_param_list(base=None, increm=None, list_len=None):
-    return None
+def make_param_list(base=None, increm=None, variation=None):
+    start_val = base - variation
+    stop_val = base + variation + 1
+    param_list = []
+    for value in range(start_val, stop_val, increm):
+        param_list.append(value)
+    return param_list
+
+
+if __name__ == "__main__":
+    test_list = make_param_list(base=20, increm=1, variation=4)
+    for item in test_list:
+        print(item)
