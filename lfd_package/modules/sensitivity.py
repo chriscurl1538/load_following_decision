77d3e3afa8584c529114f6565c43bdce8db02209
Christopher Curl <christopher.r.curl@gmail.com>
2023-07-21 12:55:12 -0700

    initial commit

---
+++ lfd_package/modules/sensitivity.py
@@ -0,0 +1,14 @@
+"""
+Module Description:
+    This module performs a sensitivity analysis on the following Python variables:
+    - Electricity costs
+    - Fuel costs
+    - Incentives
+    - TES Size
+"""
+
+from sensitivity import SensitivityAnalyzer
+
+
+def get_param_list(base=None, increm=None, list_len=None):
+    return None
