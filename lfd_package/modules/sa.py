5925e20e5378b890c4b6cfe7aafcaf6dbb6d1605
Christopher Curl <christopher.r.curl@gmail.com>
2023-08-07 15:24:58 -0700

    re-worked make_param_list function for use in salib library

--- lfd_package/modules/sa.py
+++ lfd_package/modules/sa.py
@@ -11,12 +11,13 @@ from lfd_package.modules.__init__ import ureg, Q_
 from sensitivity import SensitivityAnalyzer
 
 
-def make_param_list(base=None, variation=None):    # TODO: Manage potential negative values
-    start_val = round(base.magnitude) - variation
-    stop_val = round(base.magnitude) + variation + 1
-    increm = round((base.magnitude - start_val) / variation)
-    param_list = []
-    value_list = range(start_val, stop_val, increm)
-    for value in value_list:
-        param_list.append(Q_(value, base.units))
-    return param_list
+def make_param_list(base=None, dev=None, allow_neg=False):
+    lower = round(base.magnitude) - dev
+    upper = round(base.magnitude) + dev
+    unit = base.units
+    if lower < 0 and allow_neg is False:
+        diff = abs(lower)
+        lower = lower + diff
+        upper = upper + diff
+    return [lower, upper], unit
+
