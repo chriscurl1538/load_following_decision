5f8cfc410ec3803890a7c4ed2eede183e223465b
Christopher Curl <christopher.r.curl@gmail.com>
2023-08-19 10:46:42 -0700

    Minor bug fixed. Docstrings updated.

--- lfd_package/modules/classes.py
+++ lfd_package/modules/classes.py
@@ -4,7 +4,10 @@ Module description:
      initialize the operating parameters of the energy generation and storage systems (CHP, TES, and AuxBoiler class)
 """
 
-import math, pathlib, pandas as pd, numpy as np
+import math
+import pathlib
+import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 from lfd_package.modules.__init__ import ureg, Q_
 
@@ -13,11 +16,12 @@ class EnergyDemand:
     def __init__(self, file_name='default_file.csv', city=None, state=None, grid_efficiency=None,
                  summer_start_inclusive=None, winter_start_inclusive=None, sim_ab_efficiency=None):
         """
-        Docstring updated on 9/24/22
-
         This class stores information from EnergyPlus building demand profile simulations,
         which are fed in via a .csv file. By default, this class imports data from the
         'default_file.csv' file in the /input_files folder.
+
+        This class also stores information passed from the .yaml file associated with the
+        city, state location being analyzed.
 
         Parameters
         ----------
@@ -37,7 +41,7 @@ class EnergyDemand:
         winter_start_inclusive: int
             See above.
         sim_ab_efficiency: float
-            User must enter the assumed EnergyPlus boiler efficiency in the .yaml file so it
+            User must enter the assumed EnergyPlus boiler efficiency in the .yaml file, so it
             may be modified as needed.
         """
         # Reads load profile data from .csv file
@@ -56,7 +60,7 @@ class EnergyDemand:
             heating_metering_df = df["Gas:Facility [J](Hourly) "]
         heating_metering_hourly = heating_metering_df.to_numpy()
 
-        # Plucks month numbers from metering data file     # TODO: Test
+        # Plucks month numbers from metering data file
         meter_dates_array = df["Date/Time"].to_numpy(dtype=str)
         meter_months_hourly = []
         for item in meter_dates_array:
@@ -160,7 +164,7 @@ class EnergyDemand:
         total_energy = avg_dem * annual_hours
         return total_energy
 
-    def seasonal_weights_hourly_data(self, dem_profile=None):     # TODO: Test
+    def seasonal_weights_hourly_data(self, dem_profile=None):
         summer_start = int(self.summer_start_month)
         winter_start = int(self.winter_start_month)
         month_list = self.meter_months_hourly
@@ -176,16 +180,21 @@ class EnergyDemand:
         summer_weight_list = []
         for index in summer_index_list:
             # TODO: Unit issue - need to be consistent in passing power or energy
-            item = dem_profile[index] * Q_(1, ureg.hours)
+            item = (dem_profile[index] * Q_(1, ureg.hours)).to_reduced_units()
             summer_weight_list.append(item)
 
         winter_weight_list = []
         for index in winter_index_list:
-            item = dem_profile[index] * Q_(1, ureg.hours)
+            item = (dem_profile[index] * Q_(1, ureg.hours)).to_reduced_units()
             winter_weight_list.append(item)
 
         summer_sum = sum(summer_weight_list)
         winter_sum = sum(winter_weight_list)
+
+        if summer_sum == 0:
+            summer_sum = Q_(summer_sum, winter_sum.units)
+        elif winter_sum == 0:
+            winter_sum = Q_(winter_sum, summer_sum.units)
         total = (sum(dem_profile) * Q_(1, ureg.hours)).to_reduced_units()
         assert math.isclose(summer_sum.magnitude + winter_sum.magnitude, total.magnitude)
 
@@ -241,10 +250,17 @@ class EnergyDemand:
                 month_demand_list.append(dem_profile[index])
         return monthly_peak_list
 
-    def monthly_energy_sums(self, dem_profile=None):
+    def monthly_energy_sums(self, dem_profile=None):    # TODO: Fix bug - units issue when energy list is passed
         month_list = self.meter_months_hourly
         month_demand_list = []
         monthly_sum_list = []
+
+        # Check units, convert energy list to power list
+        if dem_profile[0].check('[energy]'):
+            i = (dem_profile[0] / Q_(1, ureg.hours)).to_reduced_units()
+            units_to = i.units
+            dem_profile = self.convert_units(values_list=dem_profile, units_to_str=str(units_to))
+
         for index, item in enumerate(month_list):
             if index == 0 or month_list[index] == month_list[index - 1]:
                 month_demand_list.append(dem_profile[index])
@@ -260,11 +276,15 @@ class EnergyDemand:
 class Emissions(EnergyDemand):
     def __init__(self, file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
                  sim_ab_efficiency):
+        """
+        Stores emission intensity values for natural gas and from electricity sub-grids
+        for each of the 7 accepted locations.
+        """
         super().__init__(file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
                          sim_ab_efficiency)
 
         # NG Emissions
-        self.ng_co2 = 14.43 * (ureg.kg / ureg.megaBtu)  # source: https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references)
+        self.ng_co2 = 14.43 * (ureg.kg / ureg.megaBtu)
 
         # Average Emissions (accounts for losses)
         self.avg_emissions = {
@@ -275,7 +295,7 @@ class Emissions(EnergyDemand):
             "duluth, mn": Q_(1040.6, ureg.lbs / ureg.MWh),
             "international falls, mn": Q_(1040.6, ureg.lbs / ureg.MWh),
             "phoenix, az": Q_(855.8, ureg.lbs / ureg.MWh),
-            "tuscon, az": Q_(855.8, ureg.lbs / ureg.MWh),
+            "tucson, az": Q_(855.8, ureg.lbs / ureg.MWh),
             "fairbanks, ak": Q_(1114.7, ureg.lbs / ureg.MWh),
             "chicago, il": Q_(1093.2, ureg.lbs / ureg.MWh),
             "buffalo, ny": Q_(243.6, ureg.lbs / ureg.MWh),
@@ -285,9 +305,37 @@ class Emissions(EnergyDemand):
 
 class EnergyCosts(EnergyDemand):
     def __init__(self, file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
-                 sim_ab_efficiency, meter_type_el=None, meter_type_fuel=None, schedule_type_el=None, schedule_type_fuel=None,
+                 sim_ab_efficiency, meter_type_el=None, meter_type_fuel=None, schedule_type_el=None, no_apts=None,
                  master_metered_el=None, single_metered_el=None, master_metered_fuel=None, single_metered_fuel=None,
-                 no_apts=None):
+                 schedule_type_fuel=None,):
+        """
+        This class defines the electricity and natural gas rate schedules. The rate schedule information is passed
+        from the .yaml file for the location being assessed.
+
+        Parameters
+        ----------
+        meter_type_el: str
+            contains string representing whether the building is master-metered
+            or sub-metered for electricity.
+        meter_type_fuel: str
+            contains string representing whether the building is master-metered
+            or sub-metered for natural gas.
+        schedule_type_el: list
+            contains string values representing the electricity rate schedule(s).
+        schedule_type_fuel: list
+            contains string values representing the natural gas rate schedule(s).
+        master_metered_el: dict
+            contains dictionary of electricity rate values for master-metered buildings.
+        single_metered_el: dict
+            contains dictionary of electricity rate values for sub-metered buildings.
+        master_metered_fuel: dict
+            contains dictionary of natural gas rate values for master-metered buildings.
+        single_metered_fuel: dict
+            contains dictionary of natural gas rate values for sub-metered buildings.
+        no_apts: int
+            contains number of apartments in the building. Used when calculating
+            monthly and annual base costs.
+        """
         super().__init__(file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
                          sim_ab_efficiency)
 
@@ -300,7 +348,7 @@ class EnergyCosts(EnergyDemand):
         self.meter_type_fuel = meter_type_fuel
         self.schedule_type_el = schedule_type_el
         self.schedule_type_fuel = schedule_type_fuel
-        self.no_apts = no_apts
+        self.no_apts = int(no_apts)
 
         # Dictionaries of Rates
         self.master_meter_el_dict = master_metered_el
@@ -311,22 +359,22 @@ class EnergyCosts(EnergyDemand):
 
 class CHP(EnergyDemand):
     def __init__(self, file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
-                 sim_ab_efficiency, turn_down_ratio=None, installed_cost=None):
+                 sim_ab_efficiency, turn_down_ratio=None, chp_installed_cost=None, chp_om_cost=None):
         """
-        Docstring updated on 9/24/22
-
-        This class defines the operating parameters of the CHP system.
+        This class defines the specifications and costs of the CHP system.
 
         Parameters
         ----------
+        chp_installed_cost: Quantity
+            contains the labor, material, and installation cost for the CHP
+            unit. Units are in $/kW.
+        chp_om_cost: Quantity
+            contains the operation and maintenance costs of the CHP unit.
+            Units are in $/kWh generated.
         turn_down_ratio: float
             The turn down ratio of the CHP system. Defines the lower operating limit
             that CHP can operate at. Dimensionless. This value is converted to a
             decimal percentage of capacity (ie: 50% is 0.5) before being stored.
-        cost: Quantity (float)
-            Incremental installed cost of CHP with units of $/kW. This value is converted
-            to a dimensionless quantity representing the installed cost of the system
-            before being stored within the class. TODO: Is this material + installation labor?
         """
 
         super().__init__(file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
@@ -343,26 +391,27 @@ class CHP(EnergyDemand):
         self.min_pl = chp_min_pl
 
         # Labor, material, and installation costs (installed cost)
-        self.installed_cost = installed_cost * 1/ureg.kW
+        self.installed_cost = chp_installed_cost * 1/ureg.kW
+        self.om_cost = chp_om_cost * 1/ureg.kWh
 
 
 class TES(EnergyDemand):
     def __init__(self, file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
-                 sim_ab_efficiency, start=None, vol_rate=None, energy_density=None, size_cost=None, rate_cost=None):
+                 sim_ab_efficiency, start=None, vol_rate=None, energy_density=None, tes_installed_cost=None):
         """
-        Docstring updated on 9/24/22
-
-        This class defines the operating parameters of the TES (thermal energy storage) system
+        This class defines the specifications and costs of the TES (thermal energy storage) system.
 
         Parameters
         ----------
         start: Quantity (float)
             The starting energy level of the TES system when the simulation begins in terms of SOC
-        cost: Quantity (float)
-            The incremental cost of the system in units of $/kWh. This value is converted to
-            a dimensionless Quantity representing installed cost before being stored
-            TODO: Add incremental cost for power system ($/kW) and combine with system_cost,
-             then store as total cost
+        vol_rate: Quantity
+            The volumetric flow rate of the TES system. Units are in liters/hr.
+        energy_density: Quantity
+            The energy density of the fluid being stored (water). Units are in kWh/m3.
+        tes_installed_cost: Quantity
+            contains the labor, material, and installation cost for the TES
+            unit. Units are in $/kWh stored.
         """
         super().__init__(file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
                          sim_ab_efficiency)
@@ -374,20 +423,16 @@ class TES(EnergyDemand):
         self.start = float(start)
         self.vol_rate = float(vol_rate) * (ureg.liters / ureg.hours)
         self.energy_density = float(energy_density) * (ureg.kWh / ureg.meters**3)
-        self.discharge_kw = (self.vol_rate * self.energy_density).to(ureg.kW)   # TODO: Implement
 
         # TES Materials Costs
-        self.size_cost = float(size_cost) * (1/ureg.kWh)     # TODO: raname variable?
-        self.rate_cost = float(rate_cost) * (1/ureg.kW)     # TODO: raname variable?
+        self.installed_cost = float(tes_installed_cost) * (1/ureg.kWh)
 
 
 class AuxBoiler(EnergyDemand):
     def __init__(self, file_name, city, state, grid_efficiency, summer_start_inclusive, winter_start_inclusive,
                  sim_ab_efficiency, efficiency=None):
         """
-        Docstring updated on 9/24/22
-
-        This class defines the operating parameters of the Auxiliary Boiler.
+        This class defines the specifications of the Auxiliary Boiler.
 
         Parameters
         ----------
