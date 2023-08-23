e96fd9b0d755e17b6c4bb5f988b73a9333877295
Christopher Curl <christopher.r.curl@gmail.com>
2023-08-23 16:55:46 -0700

    updated class arguments for pytest fixture

--- lfd_package/tests/conftest.py
+++ lfd_package/tests/conftest.py
@@ -12,7 +12,7 @@ def class_info():
         from yaml import Loader
 
     yaml_filename = 'seattle_wa.yaml'  # these match the "dest": dest="input"
-    cwd = pathlib.Path(__file__).parent.parent.resolve() / 'input_files'
+    cwd = pathlib.Path(__file__).parent.parent.resolve() / 'input_yaml'
 
     with open(cwd / yaml_filename) as f:
         data = yaml.load(f, Loader=Loader)
@@ -20,21 +20,20 @@ def class_info():
 
     # Class initialization using CLI arguments
     demand = classes.EnergyDemand(file_name=data['demand_filename'], city=data['city'], state=data['state'],
-                                  grid_efficiency=data['grid_efficiency'],
+                                  grid_efficiency=data['grid_efficiency'], sim_ab_efficiency=data["energy_plus_eff"],
                                   winter_start_inclusive=data['winter_start_inclusive'],
-                                  summer_start_inclusive=data['summer_start_inclusive'],
-                                  sim_ab_efficiency=data["energy_plus_eff"])
+                                  summer_start_inclusive=data['summer_start_inclusive'])
     emissions_class = classes.Emissions(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                                         grid_efficiency=data['grid_efficiency'],
+                                        sim_ab_efficiency=data["energy_plus_eff"],
                                         summer_start_inclusive=data['summer_start_inclusive'],
-                                        winter_start_inclusive=data['winter_start_inclusive'],
-                                        sim_ab_efficiency=data["energy_plus_eff"])
+                                        winter_start_inclusive=data['winter_start_inclusive'])
     costs_class = classes.EnergyCosts(file_name=data['demand_filename'], city=data['city'], state=data['state'],
-                                      grid_efficiency=data['grid_efficiency'],
+                                      grid_efficiency=data['grid_efficiency'], no_apts=data['no_apts'],
                                       winter_start_inclusive=data['winter_start_inclusive'],
                                       summer_start_inclusive=data['summer_start_inclusive'],
-                                      sim_ab_efficiency=data["energy_plus_eff"],
-                                      meter_type_el=data['meter_type_el'], meter_type_fuel=data['meter_type_fuel'],
+                                      sim_ab_efficiency=data["energy_plus_eff"], meter_type_el=data['meter_type_el'],
+                                      meter_type_fuel=data['meter_type_fuel'],
                                       schedule_type_el=data['schedule_type_el'],
                                       schedule_type_fuel=data['schedule_type_fuel'],
                                       master_metered_el=data['master_metered_el'],
@@ -45,19 +44,17 @@ def class_info():
                       grid_efficiency=data['grid_efficiency'], sim_ab_efficiency=data["energy_plus_eff"],
                       summer_start_inclusive=data['summer_start_inclusive'],
                       winter_start_inclusive=data['winter_start_inclusive'], turn_down_ratio=data['chp_turn_down'],
-                      installed_cost=data['chp_installed_cost'])
+                      chp_installed_cost=data['chp_installed_cost'], chp_om_cost=data['chp_om_cost'])
     ab = classes.AuxBoiler(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                            grid_efficiency=data['grid_efficiency'],
                            summer_start_inclusive=data['summer_start_inclusive'],
                            winter_start_inclusive=data['winter_start_inclusive'],
-                           sim_ab_efficiency=data["energy_plus_eff"],
-                           efficiency=data['ab_eff'])
+                           sim_ab_efficiency=data["energy_plus_eff"], efficiency=data['ab_eff'])
     tes = classes.TES(file_name=data['demand_filename'], city=data['city'], state=data['state'],
                       grid_efficiency=data['grid_efficiency'], sim_ab_efficiency=data["energy_plus_eff"],
-                      summer_start_inclusive=data['summer_start_inclusive'], vol_rate=data['tes_vol_rate'],
-                      winter_start_inclusive=data['winter_start_inclusive'], start=data['tes_init'],
-                      size_cost=data['tes_size_cost'], rate_cost=data['tes_rate_cost'],
-                      energy_density=data['tes_energy_density'])
+                      summer_start_inclusive=data['summer_start_inclusive'], start=data['tes_init'],
+                      winter_start_inclusive=data['winter_start_inclusive'], energy_density=data['tes_energy_density'],
+                      tes_installed_cost=data['tes_installed_cost'], tes_om_cost=data['tes_om_cost'])
 
     class_dict = {
         "demand": demand,
