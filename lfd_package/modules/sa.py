fb305a158346e1e43968bbbe461ce4669fd1fc48
Christopher Curl <christopher.r.curl@gmail.com>
2023-08-24 12:32:16 -0700

    Added condition for non-Quantity values being passed to function

--- lfd_package/modules/sa.py
+++ lfd_package/modules/sa.py
@@ -8,10 +8,15 @@ Module Description:
 """
 
 
-def make_param_list(base=None, dev=None, allow_neg=False):
-    lower = round(base.magnitude) - dev
-    upper = round(base.magnitude) + dev
-    unit = base.units
+def make_param_list(base=None, dev=None, allow_neg=False, has_units=True):
+    if has_units is True:
+        lower = base.magnitude - dev
+        upper = base.magnitude + dev
+        unit = base.units
+    else:
+        lower = base - dev
+        upper = base + dev
+        unit = None
     if lower < 0 and allow_neg is False:
         diff = abs(lower)
         lower = lower + diff
